2d94ad0ec13a5b2093ad574d100f4666e8503474
Lucas Abreu <lucassantosabreu@gmail.com>
2026-03-26 15:05:49 -0300

    ajuste do link

--- theme/home.php
+++ theme/home.php
@@ -904,7 +904,7 @@
                             });
 
                             const link = document.createElement('a');
-                            link.href = `http://localhost/estoque_beta/documento/comprovanteSaida/${response.id_movimentacao}`;
+                            link.href = `<?= url("/documento/comprovanteSaida/") ?>${response.id_movimentacao}`;
                             link.target = '_blank';
                             link.click();
 
